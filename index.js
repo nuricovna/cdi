4f4c140e6e941f68274612b7e987ac61b7292639
nuricovna01~ <nuricovna@gmail.com>
2023-12-13 15:39:58 +0300

    commit

--- index.js
+++ index.js
@@ -49,5 +49,33 @@ function clearActiveClasses() {
 }
 
 
-/* slider */
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [54.903661, 52.308097],
+    zoom: 16,
+  });
+
+  myMap.controls.remove('geolocationControl'); // удаляем геолокацию
+  myMap.controls.remove('searchControl'); // удаляем поиск
+  myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+  myMap.controls.remove('typeSelector'); // удаляем тип
+  myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+//   myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  myMap.controls.remove('rulerControl'); // удаляем контрол правил
+//   myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+  myMap.controls.remove('default');
+
+  var myPlacemark = new ymaps.Placemark([54.903661, 52.308097], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/icon.png',
+    iconImageSize: [40, 40],
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
+
+
 

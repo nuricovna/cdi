879d3635bd1c2baa82f0e7e579b82b1ff19a80fa
nuricovna01~ <nuricovna@gmail.com>
2023-12-26 14:47:51 +0300

    обновления

--- index.js
+++ index.js
@@ -18,18 +18,6 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-/* swiper_2 */
-
-// const swiper = new Swiper('.js-hero-swiper', {
-//   allowTouchMove: false,
-//   loop: true,
-//   effect: 'fade',
-//   speed: 10000,
-//   autoplay: {
-//     delay: 10000
-//   }
-// });
-
 
 /* slider */
 
